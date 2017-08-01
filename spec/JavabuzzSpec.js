258f0bb54bccf1f88746f98bc32ba06de8815d7f
Costas Kiteou <ckiteou22@gmail.com>
2017-08-01 21:18:13 +0100

    #2 Test Green: 'knows when a number is NOT divisible by 3'

--- spec/JavabuzzSpec.js
+++ spec/JavabuzzSpec.js
@@ -1,11 +1,20 @@
 describe('Javabuzz', function() {
   var javabuzz;
+  beforeEach(function() {
+    javabuzz = new Javabuzz();
+  });
 
   describe('knows what a number is', function() {
 
     it('divisible by 3', function() {
-      javabuzz = new Javabuzz();
       expect(javabuzz.isDivisibleByThree(3)).toBe(true);
+    });
+  });
+
+  describe('knows when a number is NOT', function() {
+
+    it('divisble by 3', function() {
+      expect(javabuzz.isDivisibleByThree(1)).toBe(false);
     });
   });
 });
