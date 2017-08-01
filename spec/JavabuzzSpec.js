3ec450b0c8ab303451a1402d456bfc5b513ef89b
Costas Kiteou <ckiteou22@gmail.com>
2017-08-01 22:19:13 +0100

    #7 Test Green: 'when playing, says 'Java' when a number is divisble by 3

--- spec/JavabuzzSpec.js
+++ spec/JavabuzzSpec.js
@@ -45,4 +45,10 @@ describe('Javabuzz', function() {
       expect(javabuzz.isDivisibleByFifteen(1)).toBe(false);
     });
   });
+
+  describe('when playing, says', function() {
+    it('"Java" when a number is divisible by 3', function() {
+      expect(javabuzz.says(3)).toEqual("Java");
+    });
+  });
 });
--- src/Javabuzz.js
+++ src/Javabuzz.js
@@ -1,9 +1,5 @@
 var Javabuzz = function() {};
 
-Javabuzz.prototype._isDivisibleBy = function(number, divisor) {
-  return (number % divisor === 0);
-};
-
 Javabuzz.prototype.isDivisibleByThree = function(number) {
   return this._isDivisibleBy(number, 3);
 };
@@ -14,4 +10,14 @@ Javabuzz.prototype.isDivisibleByFive = function(number) {
 
 Javabuzz.prototype.isDivisibleByFifteen = function(number) {
   return this._isDivisibleBy(number, 15);
+};
+
+Javabuzz.prototype._isDivisibleBy = function(number, divisor) {
+  return (number % divisor === 0);
+};
+
+Javabuzz.prototype.says = function(number) {
+  if (this.isDivisibleByThree(number)) {
+    return "Java";
+  }
 };
