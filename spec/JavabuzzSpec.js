9360c45c21e4da9a00fe1baf82032402717e9aae
Costas Kiteou <ckiteou22@gmail.com>
2017-08-01 21:31:45 +0100

    #6 Test Green: 'knows when a number is NOT divisible by 15'

--- spec/JavabuzzSpec.js
+++ spec/JavabuzzSpec.js
@@ -38,4 +38,11 @@ describe('Javabuzz', function() {
       expect(javabuzz.isDivisibleByFifteen(15)).toBe(true);
     });
   });
+
+  describe('knows when a number is NOT', function() {
+
+    it('divisble by 15', function() {
+      expect(javabuzz.isDivisibleByFifteen(1)).toBe(false);
+    });
+  });
 });
